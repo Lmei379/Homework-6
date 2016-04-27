1d2d40128b6b92483cfcbedc777304174a98dc99
Lmei379 <lmei@luc.edu>
2016-04-27 16:13:39 -0500

    Update hw6code.cpp

--- hw6code.cpp
+++ hw6code.cpp
@@ -1,32 +1,131 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <array>
-#include <cstdlib> //implemented to use the atoi
-#include <vector>
+#include<iostream>
+#include<fstream>
+#include<string> //huh???
+#include<vector>
+#include<array>
+#include <cstdlib> // implemented to use atoi
+#include<typeinfo>
+#include<math.h>
 
 using namespace std;
 
+bool quad_prob(int HT [], int num, int user_input)
+{
+    //int position = (num + (user_input)^2); //use user input as the number to square for quad probing?
+int load = 0; //counter
+  int h = num % sizeof(HT);
+  for ( int step = 1; HT[h] != 0; step++ ) {
+    // Check for a duplicate (if not allowed)
+    if ( num == HT[h] )
+      return false;
+    // Quadratic step to the next slot
+    h = ( h + ( step * step - step ) / 2 ) % sizeof(HT);
+  }
+  // Insert the new item
+  HT[h] = num;
+  ++load;
+  return true;
+}
+
+/*int * extraction (vector<string>file){
+string HA;
+int hAdd [file.size()]; // array that holds the hash addresses aka hash table?
+/*cout << "file size:" << file.size() << endl; // for debugging
+cout << "hAdd's size: " << sizeof(hAdd) << endl; */
+/*for (int s=0; s<file.size(); s++){
+        HA = file[s]; //insert social security number into string
+        //cout << "*" << HA << endl;//cout << HA[0] << "+" << HA[1] << endl;
+        char *a=new char[4]; //create a character array that takes in pieces of numbers from the string variable and
+        a[0]=HA[2];
+        a[1]=HA[4];
+        a[2]=HA[6];
+        a[3]=HA[7];
+        int j=atoi(a); // convert array into integers and input it in integer variable
+        //printing weird numbers
+        cout << j << endl;
+
+        hAdd[s] = j; } //adds the converted and extracted variable into array
+/*cout << "size: " << sizeof(hAdd) << endl;
+for (int g=0; g<sizeof(hAdd); g++){
+    cout << "hAdd" << hAdd[g] << endl;
+}
+return hAdd;
+}*/
+
+void write_file (int arr[]){
+    ofstream newfile; //created a new file to put/write stuff in
+    newfile.open("hashed_socials.txt"); //opens the new file
+    for (int x=0; x<sizeof(arr); x++){
+        newfile << arr[x] << ',';
+        if (x == (sizeof(arr)-1)){ //prevents comma at the last hash address
+            newfile << arr[x];
+        }
+    }
+}
+
 int main(){
+string line, temp ="";
+vector<string> ssn;
+ifstream in;
+int userI;//used for quadratic probing
 
-    vector <string> *ssn = new vector <string>; //created a new vector using pointer to allocate space, used to store the info from the file
-    //vector<string> ssn;
-    //read SSN's from the file into strings
-    ifstream in;
-    in.open("everybodys_social.txt");
-    int i = 0;
-    while(!EOF){
-        getline(in, ssn[i],',');
-        i++;
+cout << "Please enter a number between 1 and 450 million:" << endl;
+cin >> userI;
+
+in.open("everybodys_socials.txt");
+if (in.is_open()){
+while(getline(in,line)){ //goes through the file and gets the data into line
+    line += ',';
+    //cout << line << endl; //debuggging
+        for (int i=0; i<line.length(); i++){
+            //cout << typeid(line[i]).name() << " - " << endl; checking debugg, so line[i] is type char
+            //the if statement is not working -runtime error // - cleared up by using push back on vector
+            if (line[i] == ','){ // when it hits the comma, the temp with the whole social security number is added into the vector
+                ssn.push_back(temp);//empties temp once finish adding number into vector for another number
+                //cout << "Temp: " << temp << endl; debugging
+                temp = "";}
+        else {temp += line[i]; // if not a comma, add every char number into temp
+        }
     }
-    in.close();//close the file once it finishes putting all the info into vector
-
-    int *nums = new int[&ssn.size()]; //created array to store the hash addresses, used a pointer to allocate space
-    //int nums [ssn.size()]
-    //this for loop is to go through the vector and extract the digits I want for the hash addresses
-    for (int j =0; j<ssn.size(); j++){
-        ssn[j] = ssn[j][2] + ssn[j][4] + ssn[j][6] + ssn[j][7]; //extracts the string for every 3rd, 5th, 7th, and 8th part (since c++ starts from 0)
-        nums[j] = atoi(ssn[j]); //store and convert the extracted strings into ints
     }
+    //cout << "line length: " << line.length() << endl;//for debugging
+}
+else { cout << "file not found, please try again" << endl;}
 
-} //note use cstrings to fix problem
+in.close();
+
+string HA;
+int hAdd [ssn.size()]; // array that holds the hash addresses aka hash table?
+/*cout << "file size:" << file.size() << endl; // for debugging
+cout << "hAdd's size: " << sizeof(hAdd) << endl; */
+for (int s=0; s<ssn.size(); s++){
+        HA = ssn[s]; //insert social security number into string
+        //cout << "*" << HA << endl;//cout << HA[0] << "+" << HA[1] << endl;
+        char *a=new char[4]; //create a character array that takes in pieces of numbers from the string variable and
+        a[0]=HA[2];
+        a[1]=HA[4];
+        a[2]=HA[6];
+        a[3]=HA[7];
+        int j=atoi(a); // convert array into integers and input it in integer variable
+        //printing weird numbers
+        cout << j << endl;
+
+        hAdd[s] = j; }
+//extraction(ssn);
+//int arr [ssn.size()]; //arr = extraction(ssn);
+int hash_table[ssn.size()];
+int socialSN = 0;
+for (int p=0; p<ssn.size(); p++){
+    socialSN = atoi(ssn[p].c_str());
+    int k = hAdd[p];
+    if (hash_table[k] == NULL)
+    {
+        quad_prob(hAdd,k,userI);
+        //hash_table[posi] = socialSN;
+    }
+    else{
+    hash_table[k] = socialSN;}
+}
+
+write_file(hash_table);
+}
